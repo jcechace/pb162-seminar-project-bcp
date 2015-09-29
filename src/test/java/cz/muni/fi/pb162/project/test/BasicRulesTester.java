e74a03d25ce1239f7c751bc9ca6e198862ea1fb4
Jakub Cechacek <jcechace@redhat.com>
2015-09-29 09:50:25 +0200

    Proper formatting of BasicRulesTester

--- src/test/java/cz/muni/fi/pb162/project/test/BasicRulesTester.java
+++ src/test/java/cz/muni/fi/pb162/project/test/BasicRulesTester.java
@@ -9,65 +9,82 @@ import java.lang.reflect.Modifier;
  * @author Radek Oslejsek &lt;oslejsek@fi.muni.cz&gt;, Masaryk University, Faculty of Informatics
  * @author Petr Adamek
  */
-public class BasicRulesTester extends junit.framework.Assert {
+public class BasicRulesTester extends org.junit.Assert {
 
     private static void validateIdentifier(String identifier) {
-	assertTrue("Indentifikator " + identifier, 
-                identifier.charAt(0) == identifier.toLowerCase().charAt(0));
-	assertFalse("Indentifikator " + identifier, identifier.contains("_"));
+        assertTrue("Indentifikator " + identifier,
+            identifier.charAt(0) == identifier.toLowerCase().charAt(0));
+        assertFalse("Indentifikator " + identifier, identifier.contains("_"));
         assertFalse("Indentifikator " + identifier, identifier.contains("-"));
     }
-    
+
     private static void validateConstIdentifier(String identifier) {
-	assertTrue("Konstanta " + identifier, identifier.equals(identifier.toUpperCase()));
+        assertTrue("Konstanta " + identifier, identifier.equals(identifier.toUpperCase()));
     }
-    
+
+    /**
+     * Test the visibility of attributes
+     * @param clazz tested class
+     */
     public static void testAttributes(Class clazz) {
-	
-	for (Field field : clazz.getDeclaredFields()) {
-	    int mod = field.getModifiers();
-	    if (Modifier.isStatic(mod) && Modifier.isFinal(mod)) {
-		// constant
-		validateConstIdentifier(field.getName());
-	    } else {
-		// regular attribute
-		assertTrue("Viditelnost atributu", Modifier.isPrivate(mod));
-		validateIdentifier(field.getName());
-	    }
-	}
+
+        for (Field field : clazz.getDeclaredFields()) {
+            int mod = field.getModifiers();
+            if (Modifier.isStatic(mod) && Modifier.isFinal(mod)) {
+                // constant
+                validateConstIdentifier(field.getName());
+            } else {
+                // regular attribute
+                assertTrue("Viditelnost atributu", Modifier.isPrivate(mod));
+                validateIdentifier(field.getName());
+            }
+        }
     }
-    
+
+    /**
+     * Validate method names
+     * @param clazz tested class
+     */
     public static void testMethods(Class clazz) {
-	
-	for (Method method : clazz.getDeclaredMethods()) {
-	    validateIdentifier(method.getName());
-	}
+        for (Method method : clazz.getDeclaredMethods()) {
+            validateIdentifier(method.getName());
+        }
     }
-    
-    public static void testMethodsAndAttributes(Class clazz) {
-	testAttributes(clazz);
-	testMethods(clazz);
-    }
-    
+
+    /**
+     * Check that class has a static method with required signature
+     * @param clazz tested class
+     * @param name method name
+     * @param paramTypes parameter types
+     */
     @SuppressWarnings("unchecked")
     public static void testAbstractMethod(Class clazz, String name, Class<?>... paramTypes) {
         Method method = null;
         try {
             method = clazz.getMethod(name, paramTypes);
         } catch (NoSuchMethodException ex) {
-            fail("Trida " + clazz.getSimpleName() + " nema pozadovanou metodu " + 
-                    getMethodSignature(name, paramTypes));
+            fail("Trida " + clazz.getSimpleName() + " nema pozadovanou metodu "
+                + getMethodSignature(name, paramTypes));
         }
         assertTrue("Metoda " + name + " ma byt abstraktni",
-                Modifier.isAbstract(method.getModifiers()) || 
+            Modifier.isAbstract(method.getModifiers()) ||
                 !method.getDeclaringClass().equals(clazz));
     }
-    
+
+    /**
+     * Convenience method combining {@link  #testAttributes(Class)} and {@link #testMethods(Class)}
+     * @param clazz tested class
+     */
+    public static void testMethodsAndAttributes(Class clazz) {
+        testAttributes(clazz);
+        testMethods(clazz);
+    }
+
     /**
      * Tests presence of the implementation of non-abstract method in a class
-     * @param clazz
-     * @param name
-     * @param paramTypes 
+     * @param clazz tested class
+     * @param name name of the tested method
+     * @param paramTypes parameter types of the tested method
      */
     @SuppressWarnings("unchecked")
     public static void testNonAbstractMethod(Class clazz, String name, Class<?>... paramTypes) {
@@ -75,46 +92,58 @@ public class BasicRulesTester extends junit.framework.Assert {
         try {
             method = clazz.getMethod(name, paramTypes);
         } catch (NoSuchMethodException ex) {
-            fail("Trida " + clazz.getSimpleName() + " nema pozadovanou metodu " + 
-                    getMethodSignature(name, paramTypes));
+            fail("Trida " + clazz.getSimpleName() + " nema pozadovanou metodu " +
+                getMethodSignature(name, paramTypes));
         }
         assertFalse("Metoda " + name + " nema byt abstraktni", Modifier.isAbstract(method.getModifiers()));
     }
-    
+
+    /**
+     * Check that class has a static method with required signature
+     * @param clazz tested class
+     * @param name method name
+     * @param paramTypes parameter types
+     */
     @SuppressWarnings("unchecked")
     public static void testStaticMethod(Class clazz, String name, Class<?>... paramTypes) {
         Method method = null;
         try {
             method = clazz.getMethod(name, paramTypes);
         } catch (NoSuchMethodException ex) {
-            fail("Trida " + clazz.getSimpleName() + " nema pozadovanou metodu " + 
-                    getMethodSignature(name, paramTypes));
+            fail("Trida " + clazz.getSimpleName() + " nema pozadovanou metodu " +
+                getMethodSignature(name, paramTypes));
         }
         assertTrue("Metoda " + name + " ma byt staticka", Modifier.isStatic(method.getModifiers()));
     }
-    
+
     /**
      * Fails if the given class declares some of the given methods.
-     * 
-     * @param clazz
-     * @param name
-     * @param paramTypes 
+     *
+     * @param clazz tested class
+     * @param name name of the tested method
+     * @param paramTypes parameter types of the tested method
      */
     @SuppressWarnings("unchecked")
     public static void testRedundantMethod(Class clazz, String name, Class<?>... paramTypes) {
         Method method = null;
         try {
             method = clazz.getDeclaredMethod(name, paramTypes);
-            fail("Trida " + clazz.getSimpleName() + " nema mit metodu " + 
-                    getMethodSignature(name, paramTypes));
+            fail("Trida " + clazz.getSimpleName() + " nema mit metodu " +
+                getMethodSignature(name, paramTypes));
         } catch (NoSuchMethodException ex) {
             // ok
         }
     }
-    
+
+    /**
+     * Tests that class contains a proper main method
+     * @param clazz tested class
+     */
     public static void testRunnableClass(Class clazz) {
-        for (Method method: clazz.getMethods()) {
-            if (!method.getName().equals("main")) continue;
+        for (Method method : clazz.getMethods()) {
+            if (!method.getName().equals("main")) {
+                continue;
+            }
             assertTrue("Spustitelna trida", Modifier.isStatic(method.getModifiers()));
             assertTrue("Spustitelna trida", Modifier.isPublic(method.getModifiers()));
             assertTrue("Spustitelna trida", method.getReturnType().equals(void.class));
@@ -127,7 +156,7 @@ public class BasicRulesTester extends junit.framework.Assert {
         }
         fail("Spustitelna trida");
     }
-    
+
     @SuppressWarnings("unchecked")
     protected static String getMethodSignature(String name, Class<?>... paramTypes) {
         String m = "";
@@ -135,15 +164,22 @@ public class BasicRulesTester extends junit.framework.Assert {
             for (int i = 0; i < paramTypes.length; i++) {
                 Class<?> p = paramTypes[i];
                 m += p.getSimpleName();
-                if (i < paramTypes.length - 1) m += ", ";
+                if (i < paramTypes.length - 1) {
+                    m += ", ";
+                }
             }
             m += ")";
         }
         return name + "(" + m + ")";
     }
-    
+
+    /**
+     * Tests inheritance
+     * @param ancestor ancestor class
+     * @param clazz class
+     */
     public static void testAncestor(Class ancestor, Class clazz) {
         assertEquals("Trida " + clazz + " neni primym potomkem tridy " + ancestor,
-		ancestor,clazz.getSuperclass());
+            ancestor, clazz.getSuperclass());
     }
 }
