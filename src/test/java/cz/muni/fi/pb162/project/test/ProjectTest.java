43aeebb1977c2752941bc510ea73a41d4350bb0d
Jakub Cechacek <jcechace@redhat.com>
2015-10-11 11:32:38 +0200

    Initial assignment of iteration 03

--- src/test/java/cz/muni/fi/pb162/project/test/ProjectTest.java
+++ src/test/java/cz/muni/fi/pb162/project/test/ProjectTest.java
@@ -2,10 +2,14 @@ package cz.muni.fi.pb162.project.test;
 
 import cz.muni.fi.pb162.project.test.BasicRulesTester;
 import cz.muni.fi.pb162.project.test.OutputTester;
-import cz.muni.fi.pb162.project.demo.Demo;
-import cz.muni.fi.pb162.project.geometry.Circle;
-import cz.muni.fi.pb162.project.geometry.Triangle;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import cz.muni.fi.pb162.project.geometry.Vertex2D;
+import cz.muni.fi.pb162.project.geometry.Circle;
+import cz.muni.fi.pb162.project.geometry.Gauger;
+import cz.muni.fi.pb162.project.geometry.Measurable;
+import cz.muni.fi.pb162.project.geometry.Solid;
+import cz.muni.fi.pb162.project.geometry.Triangle;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -16,65 +20,62 @@ import org.junit.Test;
 public class ProjectTest {
     
     @Test public void task01() {
-        Vertex2D v1 = new Vertex2D(-1,0);
-        assertTrue("Konstruktor Vertex2D", v1.getX() == -1.0 && v1.getY() == 0.0);
+        assertTrue("Trida Triangle neimplementuje pozadovane rozhrani -- chyba v hlavicce tridy", Solid.class.isAssignableFrom(Triangle.class));
+        assertTrue("Trida Circle neimplementuje pozadovane rozhrani -- chyba v hlavicce tridy", Solid.class.isAssignableFrom(Circle.class));
 
-        BasicRulesTester.testRedundantMethod(Vertex2D.class, "setX", Double.TYPE);
-        BasicRulesTester.testRedundantMethod(Vertex2D.class, "setY", Double.TYPE);
+        Solid tri = new Triangle(new Vertex2D(-3,-1), new Vertex2D(-2,-2), new Vertex2D(-1,-1));
+        Solid cir = new Circle();
         
-        double dist = new Vertex2D(-1.2, 1.2).distance(new Vertex2D(1.3, 1.3));
-        assertTrue("distance() pocita spatne", dist > 2.5 && dist < 2.503);
-        try {
-            assertTrue("distance() nevraci -1 jako indikator chybneho argumentu", new Vertex2D(-1,0).distance(null) == -1.0);
-        } catch (Exception ex) {
-            fail("distance() neosetruje null argument");
-        }
+        assertTrue("Triangle.getWidth()", tri.getWidth() == 2.0);
+        assertTrue("Triangle.getHeight()", tri.getHeight() == 1.0);
+        assertTrue("Triangle.getLength()", Math.abs(tri.getLength() - 4.82842712474619) < 0.001);
+        assertTrue("Triangle.getArea()", Math.abs(tri.getArea() - 1.0) < 0.001);
+        
+        assertTrue("Circle.getWidth()", cir.getWidth() == 2.0);
+        assertTrue("Circle.getHeight()", cir.getHeight() == 2.0);
+        assertTrue("Circle.getLength()", Math.abs(cir.getLength() - (Math.PI * 2.0)) < 0.001);
+        assertTrue("Circle.getArea()", Math.abs(cir.getArea() - Math.PI) < 0.001);
     }
 
     @Test public void task02() {
-        Triangle tri = new Triangle(new Vertex2D(-1,0), new Vertex2D(1,0), new Vertex2D(0,1));
-        assertTrue("Konstruktor Triangle", tri.getVertexA().getX() == -1.0 && tri.getVertexA().getY() == 0.0);
+        BasicRulesTester.testStaticMethod(Gauger.class, "printMeasurement", Measurable.class);
+        BasicRulesTester.testStaticMethod(Gauger.class, "printMeasurement", Solid.class);
         
-        BasicRulesTester.testRedundantMethod(Triangle.class, "setVertexA", Vertex2D.class);
-        BasicRulesTester.testRedundantMethod(Triangle.class, "setVertexB", Vertex2D.class);
-        BasicRulesTester.testRedundantMethod(Triangle.class, "setVertexC", Vertex2D.class);
+        OutputTester ot = new OutputTester();
+        ot.captureOutput();
+        Gauger.printMeasurement((Measurable)new Circle(new Vertex2D(0,0), 1));
+        boolean eq = ot.outputEquals("Circle: center=[0.0, 0.0], radius=1.0" +
+                System.getProperty("line.separator") +
+                "Length/Perimeter: 6.283185307179586");
+        ot.releaseOutput();
+        assertTrue("Spatny vystup metody Gauger.printMeasurement(Measurable)", eq);
         
-        assertTrue("isEquilateral()", new Triangle(new Vertex2D(-1.2, 0), new Vertex2D(1.2, 0), new Vertex2D(0, 2.07846097)).isEquilateral());
-        assertFalse("isEquilateral()", new Triangle(new Vertex2D(-1.2, 0), new Vertex2D(1.2, 0), new Vertex2D(0, 5.0)).isEquilateral());
+        ot = new OutputTester();
+        ot.captureOutput();
+        Gauger.printMeasurement((Solid) new Circle(new Vertex2D(0,0), 1));
+        eq = ot.outputEquals("Circle: center=[0.0, 0.0], radius=1.0" +
+                System.getProperty("line.separator") +
+                "Length/Perimeter: 6.283185307179586" +
+                System.getProperty("line.separator") +
+                "Area: 3.141592653589793");
+        ot.releaseOutput();
+        assertTrue("Spatny vystup metody Gauger.printMeasurement(Solid)", eq);
         
-        try {
-            Triangle ntri = new Triangle(null, null, null);
-            assertEquals("toString vraci spatny vystup pokud je nektery z vrcholu prazdny", "INVALID TRIANGLE", ntri.toString());
-        } catch(NullPointerException ex) {
-            fail("toString neosetruje situaci, kdy je nektery z vrcholu prazdny");
-        }
+        System.err.println("Cvicici zkontroluje vzajemne volani metod v Gauer [-0.5 bodu]");
     }
 
     @Test public void task03() {
-        // task02 otestuje, zda byl pridan konstruktor a zda byly zruseny settry.
-        // Zbyva tedy otestovat vystup (aby studenti pouze nesmazali telo metody main).
-        OutputTester ot = new OutputTester();
-        ot.captureOutput();
-        Demo.main(null);
-        boolean eq = ot.outputEquals("Triangle: vertices=[-100.0, 0.0] [0.0, 100.0] [100.0, -100.0]");
-        ot.releaseOutput();
-        assertTrue("Spatny vystup metody Demo.main()", eq);
-    }
-
-    @Test public void task04() {
-        Circle c1 = new Circle();
-        Circle c2 = new Circle(new Vertex2D(1.0, -1.0), 0.5);
+        boolean hasStaticField = false;
+        for (Field field : Triangle.class.getDeclaredFields()) {
+            if (Modifier.isStatic(field.getModifiers())) {
+                hasStaticField = true;
+                break;
+            }
+        }
+        if (!hasStaticField) fail("Epsilon neni verejna konstanta");
+        BasicRulesTester.testAttributes(Triangle.class);
         
-        assertTrue("Parametricky konstruktor: spatny polomer", c2.getRadius() == 0.5);
-        assertTrue("Parametricky konstruktor: spatny stred", c2.getCenter().getX() == 1.0 && c2.getCenter().getY() == -1.0);
-        assertTrue("Bezparametricky konstruktor: spatny polomer", c1.getRadius() == 1.0);
-        assertTrue("Bezparametricky konstruktor: spatny stred", c1.getCenter().getX() == 0.0 && c1.getCenter().getY() == 0.0);
-        //c2.setRadius(0.3);
-        //assertTrue("setRadius()", c2.getRadius() == 0.3);
-        
-        assertEquals("toString", "Circle: center=[1.0, -1.0], radius=0.5", c2.toString());
-
-        System.err.println("Cvicici zkontroluje vzajemne volani konstruktoru ve tride Circle! [-0.5 bodu]");
+        System.err.println("Cvicici zkontroluje pouziti konstanty v Triangle.isEquilateral() [-0.5 bodu]");
     }
 
 }
